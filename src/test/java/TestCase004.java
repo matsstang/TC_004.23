35b3322e7e6ba23de428431b209d01db3a0f5ab9
Peter <matsstang82@gmail.com>
2023-04-04 01:39:41 -0500

    first commit

---
+++ src/test/java/TestCase004.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase004 {
+    @Test
+    public void accountTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://askomdch.com");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("li#menu-item-1237")).click();
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/account/");
+        driver.quit();
+        }
+}
