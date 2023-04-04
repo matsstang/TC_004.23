7dea4d995a8d5149a142b19b89f8751c25804e5c
Peter <matsstang82@gmail.com>
2023-04-04 10:27:19 -0500

    added shopTest method

--- src/test/java/TestCase005.java
+++ src/test/java/TestCase005.java
@@ -4,16 +4,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestCase004 {
+public class TestCase005 {
     @Test
-    public void accountTest() throws InterruptedException {
+    public void shopTest() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://askomdch.com");
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("li#menu-item-1237")).click();
+        driver.findElement(By.cssSelector("a.wp-block-button__link")).click();
         Thread.sleep(2000);
-        Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/account/");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/store");
         driver.quit();
     }
 }
