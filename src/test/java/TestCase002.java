bf8f7c610b0354778483b83c4ead290cc55fcfa8
Peter <matsstang82@gmail.com>
2023-04-04 14:03:57 -0500

    added currencyTest method

--- src/test/java/TestCase002.java
+++ src/test/java/TestCase002.java
@@ -11,7 +11,16 @@ public class TestCase002 {
         driver.manage().window().maximize();
         driver.get("https://askomdch.com");
         Thread.sleep(2000);
-        Assert.assertEquals(driver.findElement(By.cssSelector("span.woocommerce-Price-currencySymbol")).getText(), "$");
+        Assert.assertEquals(driver.findElement(By.cssSelector("span.woocommerce-Price-currencySymbol")).
+                getText(), "$");
+        Assert.assertEquals(driver.findElement(By.xpath("//li[2]/div[2]/span[2]/span/bdi/span")).
+                getText(), "$");
+        Assert.assertEquals(driver.findElement(By.xpath("//li[3]/div[2]/span[2]/ins/span/bdi/span")).
+                getText(), "$");
+        Assert.assertEquals(driver.findElement(By.xpath("//li[4]/div[2]/span[2]/ins/span/bdi/span")).
+                getText(), "$");
+        Assert.assertEquals(driver.findElement(By.xpath("//li[5]/div[2]/span[2]/span/bdi/span")).
+                getText(), "$");
         driver.quit();
     }
 }
