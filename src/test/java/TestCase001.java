8009c338c978d44ada124dc285184b0272c5273c
Peter <matsstang82@gmail.com>
2023-04-07 15:38:09 -0500

    minor fixes

--- src/test/java/TestCase001.java
+++ src/test/java/TestCase001.java
@@ -11,12 +11,12 @@ public class TestCase001 {
         driver.manage().window().maximize();
         driver.get("https://askomdch.com");
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("a[href='https://askomdch.com/product/basic-blue-jeans/']")).click();
+        driver.findElement(By.cssSelector("a[href*='basic-blue-jeans']")).click();
         Thread.sleep(2000);
         Assert.assertEquals(driver.findElement(By.cssSelector("span.onsale")).getText(), "Sale!");
         driver.navigate().back();
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("a[href='https://askomdch.com/product/anchor-bracelet/']")).click();
+        driver.findElement(By.cssSelector("a[href*='anchor-bracelet']")).click();
         Thread.sleep(2000);
         Assert.assertEquals(driver.findElement(By.cssSelector("span.onsale")).getText(), "Sale!");
         driver.quit();
